855b363ec043735088590aef61b2c50030ad181e
Matt <mattdholmes@comcast.net>
2019-10-02 16:09:23 -0400

    we droppin

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -13,12 +13,34 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DropApple", 2f, appleRate);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if ( pos.x < -lrEdge )
+        {
+            speed = Mathf.Abs(speed);
+        }else if (pos.x > lrEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < directionalChange)
+        {
+            speed *= -1;
+        }
     }
 }
