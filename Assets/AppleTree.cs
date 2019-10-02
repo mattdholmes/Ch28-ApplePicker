fb47453f6d12479334d01025b8df8cbfba79e1e0
Matt <mattdholmes@comcast.net>
2019-10-02 15:31:05 -0400

    appletree script

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float lrEdge = 10f;
+    public float directionalChange = 0.1f;
+    public float appleRate = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
